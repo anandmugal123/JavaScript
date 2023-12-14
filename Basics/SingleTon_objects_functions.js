6f56dfc46b7c9a4a7414325c82ddfd482d839b8b
Anand Mugal <115776843+anandmugal123@users.noreply.github.com>
2023-12-14 12:26:42 +0000

    Switch_STATEMENT

--- Basics/SingleTon_objects_functions.js
+++ Basics/SingleTon_objects_functions.js
@@ -5,36 +5,36 @@ ExpenseSheet.date="14/12/2023";
 ExpenseSheet.Title="TODAY expense";
 ExpenseSheet.Amount="569";
 
-// console.log(ExpenseSheet);
-//  function AmountSpend(...num1){
-//      return num1
-//  }
-// console.log(AmountSpend(500,455,600,470,525,425,580));
-// function Expense(ANYOBJECT,sdf){
-//     console.log(`The Total Expense for ${ANYOBJECT.date} is ${ANYOBJECT.Amount}`);
-// }
+console.log(ExpenseSheet);
+ function AmountSpend(...num1){
+     return num1
+ }
+console.log(AmountSpend(500,455,600,470,525,425,580));
+function Expense(ANYOBJECT,sdf){
+    console.log(`The Total Expense for ${ANYOBJECT.date} is ${ANYOBJECT.Amount}`);
+}
 
-// console.log(Expense(ExpenseSheet));
-// Expense({
-//   Amount:784,
-//   date:"15/12/2023"
-// })
+console.log(Expense(ExpenseSheet));
+Expense({
+  Amount:784,
+  date:"15/12/2023"
+})
 
 const arr=[500,1200,1800,2200]
-  (function array(rt,SD){
+function array(rt,SD){
     console.log(`the EXPENSE FOR ${rt.date} is ${SD[1]}`)
-  })
+  }
 
-// console.log(array(ExpenseSheet,arr)))
-//  const emp={
-//     EmName:"Avadhut Shinde",
-//     AGE:"23",
-//     welcomemessage: function(){
-//       console.log(`${this.EmName} welcome to company`)
-//     }
+ console.log(array(ExpenseSheet,arr));
+ const emp={
+    EmName:"Avadhut Shinde",
+    AGE:"23",
+    welcomemessage: function(){
+      console.log(`${this.EmName} welcome to company`)
+    }
 
       
-//     }
-// console.log(emp.welcomemessage()); 
+}
+console.log(emp.welcomemessage()); 
 // const emp=(l,m)=>(l+m);
 // console.log(emp(78,85))
