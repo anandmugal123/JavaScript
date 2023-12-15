ad476f95ac24cc8c47b9c51f10f180f85d091e99
Anand Mugal <115776843+anandmugal123@users.noreply.github.com>
2023-12-15 07:30:33 +0000

    Loops

---
+++ Iterations/iterations.js
@@ -0,0 +1,39 @@
+let count=1;
+// let empcount=10;
+// for (let  i= 1;i<=A; i++) {
+//     if (i%2==0) {
+//         console.log(`Even Numbers are${i}`);
+//     }    
+// // }
+// for (let i=1;i<=A;i++){
+//     console.log(`Table for ${i}`);
+//     for (let j = 1; j <=10; j++) {
+//         console.log(i+"*"+j+"="+i*j);
+        
+//     }
+// 
+
+// const emp=["AVADHUT","PRATIK","ANAND"]
+// for (let itr = 0; itr < emp.length; itr++) {
+//     const rt = emp[itr];
+//     console.log(rt)
+    
+// }
+// while loop
+// while (count<=20) {
+//     console.log(count);
+//     count++;
+// }
+let empdt=["AVADHUT Shinde","Pratik Adhane","Anand Mugal"]
+let ct=0
+// while (ct<empdt.length) {
+//     console.log(`EMPLOYEE Working are ${empdt[ct]}`);
+//     ct++;
+// }
+
+// Do While Loop
+//  do {
+//      console.log("Welcome TO Datav")
+//      ct++
+//  } while (ct<5);
+ 
