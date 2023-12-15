5977c6c54694790729204d4c04a575b421851961
Anand Mugal <115776843+anandmugal123@users.noreply.github.com>
2023-12-15 11:15:35 +0000

    filter,for_of,for_each loops.

--- Iterations/iterations.js
+++ Iterations/iterations.js
@@ -24,7 +24,7 @@ let count=1;
 //     console.log(count);
 //     count++;
 // }
-let empdt=["AVADHUT Shinde","Pratik Adhane","Anand Mugal"]
+let empdt=["AVADHUT Shinde","Pratik Adhane","Anand Mugal",1,2,3,4,5,6,7,8,9,10,11,12,14,16,18,20]
 let ct=0
 // while (ct<empdt.length) {
 //     console.log(`EMPLOYEE Working are ${empdt[ct]}`);
@@ -36,4 +36,70 @@ let ct=0
 //      console.log("Welcome TO Datav")
 //      ct++
 //  } while (ct<5);
+// empdt.forEach((Name,index,empdt)=>{
+//      if (Name%2==0) {
+//         console.log(`${Name} is Even`);
+//         console.log(Name,index,empdt);   
+//      }
+        
+//     }
+// )
+
+
+
+// const  data=[
+//     {
+//         EName:"ANAND Mugal",
+//         ADress:"PUNE"
+//     },
+//     {
+//         EName:"AVADHUT Shinde",
+//         ADress:"PNQ",
+//     },
+//     {
+//         EName:"PRATIK  Adhane",
+//         ADress:"pnq",
+//     }
+// ]
  
+// for (const iter of empdt) {
+//     console.log(iter);
+// }
+
+// const Wish="Anand Mugal";
+// for (const ISH of Wish) {
+//     console.log(`Hello ${ISH} Welcome to Team`);
+    
+// }
+//filter
+// const newnumber=empdt.filter((num)=> num%2==0);
+// console.log(newnumber);
+
+// const newnumber=empdt.filter((num)=>{
+
+//     if (num%2==0) {
+//         console.log(`${num} is Even`);
+//     } else {
+//         console.log(`Not Even Number`);
+        
+//     }
+
+// })
+const books = [
+    { title: 'Book One', genre: 'Fiction', publish: 1981, edition: 2004 },
+    { title: 'Book Two', genre: 'Non-Fiction', publish: 1992, edition: 2008 },
+    { title: 'Book Three', genre: 'History', publish: 1999, edition: 2007 },
+    { title: 'Book Four', genre: 'Non-Fiction', publish: 1989, edition: 2010 },
+    { title: 'Book Five', genre: 'Science', publish: 2009, edition: 2014 },
+    { title: 'Book Six', genre: 'Fiction', publish: 1987, edition: 2010 },
+    { title: 'Book Seven', genre: 'History', publish: 1986, edition: 1996 },
+    { title: 'Book Eight', genre: 'Science', publish: 2011, edition: 2016 },
+    { title: 'Book Nine', genre: 'Non-Fiction', publish: 1981, edition: 1989 },
+  ];
+
+//   const FAV= books.filter((books)=>books.genre==='History');
+//   console.log(FAV)
+const FAV=books.filter((pb)=>{
+    return pb.edition>='2000' && pb.genre==='Science'}
+    );
+console.log(FAV);
