1f9628004d40f5d45cfb43a377270607b68cf2d1
Anand Mugal <115776843+anandmugal123@users.noreply.github.com>
2023-12-18 12:33:41 +0000

    Function containing another Function

---
+++ LET/ONE.js
@@ -0,0 +1,7 @@
+// write a function to createHelloWorld it should return new function that will always return "Hellow World"
+let createHelloWorld=function(){
+    return function(){
+         return "Hello World"
+
+    }
+}
